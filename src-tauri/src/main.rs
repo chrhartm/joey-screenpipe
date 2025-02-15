9008ac201febda1eba77c6167a4269a2b6b05751
Christoph Hartmann <christophartmann@gmail.com>
2025-02-15 14:55:24 +0000

    added menubar icon and made default invisible

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::{
+    Manager,
+    menu::{MenuBuilder,},
+    tray::{TrayIcon, TrayIconBuilder, TrayIconEvent,},
+};
+
 fn main() {
     screenpipe_tauri_template_lib::run()
 }
